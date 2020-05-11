a14e4d52a47bd2982f463f8475eeb20b151bccb6
Ryan Webb <ryan@ryanwebb.com>
2020-05-11 11:58:34 +0100

    Initial commit - prototype Tetris game

---
+++ Assets/TetrisAI/Scripts/TetrisController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisController : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+
+    private void Start()
+    {
+        NewTetronimo();
+    }
+
+    public void NewTetronimo()
+    {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
